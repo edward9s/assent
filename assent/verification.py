7f840a56bdaa1390cfd16ed5c1b88943832fd5fc
edward9s <277983297+edward9s@users.noreply.github.com>
2000-01-01 02:52:00 +0000

    auto(acceptbatch01/t001): Batch verification receipt: schema, storage, and validation

--- assent/verification.py
+++ assent/verification.py
@@ -1,8 +1,17 @@
-"""Reproducible, unattended full verification of one integration candidate.
+"""Reproducible, unattended full verification of an integration candidate.
 
-The receipt written here is a derived runtime cache.  It records facts about an
-explicit source snapshot, the resulting integration tree, and the main-tree
-verification script; it is not human approval and never advances a Git ref.
+The receipts written here are a derived runtime cache.  They record facts about
+explicit source snapshots, the resulting integration trees, and the main-tree
+verification script; they are not human approval and never advance a Git ref.
+
+Two independent receipt models live side by side and never read each other's
+files:
+
+* the per-folder receipt (``<folder>/_verification.toml``), covering one folder
+  merged into the target;
+* the batch receipt (``.assent/_batch_verification.toml``), covering one full
+  verification of several folders merged in a recorded order, so that a batch
+  release can publish them one by one against a single test run.
 """
 from __future__ import annotations
 
@@ -15,12 +24,13 @@ import sys
 import time
 import tomllib
 import uuid
+from collections.abc import Sequence
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from pathlib import Path
 
 from assent import AssentError, gitops
-from assent.config import Config
+from assent.config import Config, _validate_tasks_name
 from assent.folderdeps import (infer_folder_completion, live_upstreams,
                                parse_folder_dependencies)
 from assent.lockfile import LockBusy, hold_integration_lock, hold_lock
@@ -173,16 +183,13 @@ def _validate_receipt(receipt: VerificationReceipt, repository: Path) -> None:
                 f"Verification receipt {name} names a Git {actual}, not a {expected}")
 
 
-def write_receipt(path: Path, receipt: VerificationReceipt,
-                  repository: Path) -> None:
-    """Atomically replace a receipt after validating its schema and Git objects."""
-    path = Path(path)
-    _validate_receipt(receipt, repository)
+def _atomic_write_text(path: Path, text: str) -> None:
+    """Replace one receipt file in place, flushed and without a partial state."""
     path.parent.mkdir(parents=True, exist_ok=True)
     temporary = path.with_name(f".{path.name}.{uuid.uuid4().hex}.tmp")
     try:
         with open(temporary, "w", encoding="utf-8", newline="\n") as handle:
-            handle.write(_receipt_text(receipt))
+            handle.write(text)
             handle.flush()
             os.fsync(handle.fileno())
         os.replace(temporary, path)
@@ -193,6 +200,14 @@ def write_receipt(path: Path, receipt: VerificationReceipt,
             temporary.unlink(missing_ok=True)
         except OSError:
             pass
+
+
+def write_receipt(path: Path, receipt: VerificationReceipt,
+                  repository: Path) -> None:
+    """Atomically replace a receipt after validating its schema and Git objects."""
+    path = Path(path)
+    _validate_receipt(receipt, repository)
+    _atomic_write_text(path, _receipt_text(receipt))
 
 
 def read_receipt(path: Path, repository: Path) -> VerificationReceipt:
@@ -603,3 +618,347 @@ def receipt_report_lines(cfg: Config) -> list[str]:
     if receipt.failure_summary:
         lines.append("  Failure: " + receipt.failure_summary.splitlines()[0])
     return lines
+
+
+# --- Batch receipt: one full verification covering several folders ------------
+#
+# One candidate tree merges every queued folder in a recorded order, is verified
+# once, and is then released folder by folder.  Each intermediate merge commit
+# must be comparable against the receipt, so the receipt stores the tree after
+# every step, not only the final tree.
+
+BATCH_RECEIPT_NAME = "_batch_verification.toml"
+BATCH_RECEIPT_VERSION = 1
+_BATCH_RECEIPT_KEYS = {
+    "version", "status", "target_tip", "sources", "final_tree",
+    "verify_script_sha256", "verify_command", "exit_code", "completed_at",
+    "failure_summary",
+}
+_BATCH_SOURCE_KEYS = {"folder", "source_tip", "step_tree"}
+
+
+@dataclass(frozen=True)
+class BatchSource:
+    """One folder's place in the recorded merge order.
+
+    ``step_tree`` is the candidate tree right after this folder was merged, so a
+    release can compare every intermediate merge commit it creates, not just the
+    end of the chain.
+    """
+
+    folder: str
+    source_tip: str
+    step_tree: str
+
+
+@dataclass(frozen=True)
+class BatchVerificationReceipt:
+    """Evidence of one full verification covering an ordered list of folders."""
+
+    version: int
+    status: str
+    target_tip: str
+    sources: tuple[BatchSource, ...]
+    final_tree: str
+    verify_script_sha256: str
+    verify_command: str
+    exit_code: int
+    completed_at: str
+    failure_summary: str
+
+    def __post_init__(self) -> None:
+        # A TOML array reads back as a list; normalizing keeps a receipt equal to
+        # its own round trip without coercing the element type, which stays a
+        # validated schema error.
+        if isinstance(self.sources, list):
+            object.__setattr__(self, "sources", tuple(self.sources))
+
+    @property
+    def folders(self) -> tuple[str, ...]:
+        """Folder names in the recorded merge order."""
+        return tuple(source.folder for source in self.sources)
+
+
+@dataclass(frozen=True)
+class BatchCandidate:
+    """The rebuilt merge chain: either every step tree, or the first conflict."""
+
+    step_trees: tuple[str, ...] = ()
+    conflict_folder: str = ""
+    conflicts: tuple[str, ...] = ()
+
+    @property
+    def ok(self) -> bool:
+        return not self.conflict_folder
+
+
+def batch_receipt_path(assent_dir: str | Path) -> Path:
+    """Return the repository-level batch receipt path.
+
+    The batch receipt spans folders, so it belongs to ``.assent/`` itself and
+    never to one folder's directory.
+    """
+    return Path(assent_dir) / BATCH_RECEIPT_NAME
+
+
+def _batch_receipt_text(receipt: BatchVerificationReceipt) -> str:
+    text = (
+        f"version = {receipt.version}\n"
+        f"status = {_toml_string(receipt.status)}\n"
+        f"target_tip = {_toml_string(receipt.target_tip)}\n"
+        f"final_tree = {_toml_string(receipt.final_tree)}\n"
+        f"verify_script_sha256 = {_toml_string(receipt.verify_script_sha256)}\n"
+        f"verify_command = {_toml_string(receipt.verify_command)}\n"
+        f"exit_code = {receipt.exit_code}\n"
+        f"completed_at = {_toml_string(receipt.completed_at)}\n"
+        f"failure_summary = {_toml_string(receipt.failure_summary)}\n"
+    )
+    for source in receipt.sources:
+        text += (
+            "\n[[sources]]\n"
+            f"folder = {_toml_string(source.folder)}\n"
+            f"source_tip = {_toml_string(source.source_tip)}\n"
+            f"step_tree = {_toml_string(source.step_tree)}\n"
+        )
+    return text
+
+
+def _require_oid(value: object, name: str) -> None:
+    if not isinstance(value, str) or not _OID_RE.fullmatch(value):
+        raise AssentError(
+            f"Batch verification receipt {name} must be a 40- or 64-character "
+            "lowercase hexadecimal object id")
+
+
+def _validate_batch_receipt(receipt: BatchVerificationReceipt,
+                            repository: Path) -> None:
+    """Fail closed on any incomplete, inconsistent, or non-existent identity."""
+    if type(receipt.version) is not int or receipt.version != BATCH_RECEIPT_VERSION:
+        raise AssentError(
+            f"Batch verification receipt version must be {BATCH_RECEIPT_VERSION}")
+    if receipt.status not in _RECEIPT_STATUSES:
+        raise AssentError(
+            "Batch verification receipt status must be PASSED or FAILED")
+    _require_oid(receipt.target_tip, "target_tip")
+    _require_oid(receipt.final_tree, "final_tree")
+    if not isinstance(receipt.sources, tuple) or not receipt.sources:
+        raise AssentError(
+            "Batch verification receipt sources must be a non-empty ordered list")
+    seen: set[str] = set()
+    for index, source in enumerate(receipt.sources):
+        if not isinstance(source, BatchSource):
+            raise AssentError(
+                f"Batch verification receipt sources[{index}] is not a source entry")
+        if not isinstance(source.folder, str):
+            raise AssentError(
+                f"Batch verification receipt sources[{index}] folder must be a string")
+        _validate_tasks_name(source.folder, "Batch verification receipt folder")
+        if source.folder in seen:
+            raise AssentError(
+                "Batch verification receipt lists folder "
+                f"{source.folder} more than once")
+        seen.add(source.folder)
+        _require_oid(source.source_tip, f"sources[{index}] source_tip")
+        _require_oid(source.step_tree, f"sources[{index}] step_tree")
+    if receipt.final_tree != receipt.sources[-1].step_tree:
+        raise AssentError(
+            "Batch verification receipt final_tree must equal the last step_tree")
+    if not isinstance(receipt.verify_script_sha256, str) or not _DIGEST_RE.fullmatch(
+            receipt.verify_script_sha256):
+        raise AssentError(
+            "Batch verification receipt verify_script_sha256 must be a "
+            "64-character lowercase hexadecimal digest")
+    if receipt.verify_command != VERIFY_COMMAND:
+        raise AssentError(
+            "Batch verification receipt verify_command must be "
+            f"{VERIFY_COMMAND!r}")
+    if type(receipt.exit_code) is not int:
+        raise AssentError(
+            "Batch verification receipt exit_code must be an integer")
+    if not isinstance(receipt.completed_at, str):
+        raise AssentError(
+            "Batch verification receipt completed_at must be a string")
+    try:
+        completed = datetime.fromisoformat(receipt.completed_at)
+    except ValueError as e:
+        raise AssentError(
+            "Batch verification receipt completed_at must be ISO 8601") from e
+    if completed.tzinfo is None:
+        raise AssentError(
+            "Batch verification receipt completed_at must include a timezone")
+    if not isinstance(receipt.failure_summary, str):
+        raise AssentError(
+            "Batch verification receipt failure_summary must be a string")
+    if len(receipt.failure_summary) > _SUMMARY_LIMIT:
+        raise AssentError(
+            "Batch verification receipt failure_summary exceeds the size limit")
+    if receipt.status == "PASSED" and receipt.exit_code != 0:
+        raise AssentError(
+            "A PASSED batch verification receipt must have exit_code = 0")
+    if receipt.status == "FAILED" and receipt.exit_code == 0:
+        raise AssentError(
+            "A FAILED batch verification receipt must have a nonzero exit_code")
+    expected = [(receipt.target_tip, "commit"), (receipt.final_tree, "tree")]
+    for source in receipt.sources:
+        expected.append((source.source_tip, "commit"))
+        expected.append((source.step_tree, "tree"))
+    for object_id, wanted in expected:
+        actual = gitops.object_type(repository, object_id)
+        if actual != wanted:
+            raise AssentError(
+                f"Batch verification receipt {object_id} names a Git {actual}, "
+                f"not a {wanted}")
+
+
+def write_batch_receipt(path: Path, receipt: BatchVerificationReceipt,
+                        repository: Path) -> None:
+    """Atomically replace the batch receipt after validating schema and objects."""
+    path = Path(path)
+    _validate_batch_receipt(receipt, repository)
+    _atomic_write_text(path, _batch_receipt_text(receipt))
+
+
+def read_batch_receipt(path: Path,
+                       repository: Path) -> BatchVerificationReceipt:
+    """Read the batch receipt fail-closed, including Git existence and types."""
+    path = Path(path)
+    try:
+        with open(path, "rb") as handle:
+            data = tomllib.load(handle)
+    except FileNotFoundError as e:
+        raise AssentError(f"Batch verification receipt not found: {path}") from e
+    except OSError as e:
+        raise AssentError(
+            f"Unable to read batch verification receipt {path}: {e}") from e
+    except tomllib.TOMLDecodeError as e:
+        raise AssentError(
+            f"Batch verification receipt is not valid TOML ({path}): {e}") from e
+    if not isinstance(data, dict):
+        raise AssentError("Batch verification receipt must be a TOML table")
+    unknown = sorted(set(data) - _BATCH_RECEIPT_KEYS)
+    missing = sorted(_BATCH_RECEIPT_KEYS - set(data))
+    if unknown:
+        raise AssentError(
+            f"Batch verification receipt has unknown keys: {', '.join(unknown)}")
+    if missing:
+        raise AssentError(
+            f"Batch verification receipt is missing keys: {', '.join(missing)}")
+    raw_sources = data.pop("sources")
+    if not isinstance(raw_sources, list):
+        raise AssentError(
+            "Batch verification receipt sources must be an array of tables")
+    sources: list[BatchSource] = []
+    for index, item in enumerate(raw_sources):
+        if not isinstance(item, dict):
+            raise AssentError(
+                f"Batch verification receipt sources[{index}] is not a table")
+        unknown = sorted(set(item) - _BATCH_SOURCE_KEYS)
+        missing = sorted(_BATCH_SOURCE_KEYS - set(item))
+        if unknown:
+            raise AssentError(
+                f"Batch verification receipt sources[{index}] has unknown keys: "
+                f"{', '.join(unknown)}")
+        if missing:
+            raise AssentError(
+                f"Batch verification receipt sources[{index}] is missing keys: "
+                f"{', '.join(missing)}")
+        sources.append(BatchSource(**item))
+    try:
+        receipt = BatchVerificationReceipt(sources=tuple(sources), **data)
+    except TypeError as e:
+        raise AssentError(
+            f"Batch verification receipt has an invalid structure: {e}") from e
+    _validate_batch_receipt(receipt, repository)
+    return receipt
+
+
+def build_batch_candidate(main: Path, target_tip: str,
+                          sources: Sequence[tuple[str, str]]) -> BatchCandidate:
+    """Merge every ``(folder, source_tip)`` in order and return each step tree.
+
+    The chain is built in one temporary worktree that is always removed, and the
+    first conflicting folder stops it.  Every step is a no-fast-forward merge, so
+    the trees recorded here are exactly the trees a release reproduces.
+    """
+    if not sources:
+        raise AssentError("a batch candidate needs at least one source folder")
+    step_trees: list[str] = []
+    with gitops.temporary_integration_worktree(
+            main, "batch", target_tip) as (candidate, _branch):
+        for folder, source_tip in sources:
+            message = f"verify(batch/{folder}): temporary integration candidate"
+            outcome = gitops.merge_no_ff(candidate, source_tip, message)
+            if not outcome.ok:
+                return BatchCandidate(
+                    tuple(step_trees), folder, tuple(outcome.conflicts))
+            step_trees.append(gitops.tree_of(candidate, "HEAD"))
+    return BatchCandidate(tuple(step_trees))
+
+
+def batch_receipt_staleness(cfg: Config,
+                            receipt: BatchVerificationReceipt) -> tuple[str, ...]:
+    """Return every reason the whole batch has expired; empty means still current.
+
+    A batch receipt is all-or-nothing: any one drifted source expires the batch,
+    because the recorded merge order and its step trees only describe that exact
+    set of sources.  Cheap identity checks run first; the merge chain is rebuilt
+    only when every recorded identity is still current, and the target tip itself
+    is deliberately not compared -- a target that moved without changing the
+    rebuilt trees keeps the receipt usable.
+    """
+    main = gitops.main_worktree(cfg.root)
+    reasons: list[str] = []
+    try:
+        if verifier_digest(cfg) != receipt.verify_script_sha256:
+            reasons.append("verification script changed")
+    except AssentError as e:
+        reasons.append(f"verification script is unavailable: {e}")
+
+    target_tip = gitops.commit_of(main, gitops.require_current_branch(main))
+    for source in receipt.sources:
+        try:
+            branch = gitops.unique_folder_branch(main, source.folder)
+        except AssentError as e:
+            reasons.append(f"source branch for {source.folder} is ambiguous: {e}")
+            continue
+        if branch is None:
+            reasons.append(f"source branch for {source.folder} no longer exists")
+            continue
+        tip = gitops.branch_tip(main, branch)
+        if tip != source.source_tip:
+            reasons.append(
+                f"source tip for {source.folder} changed from {source.source_tip} "
+                f"to {tip}")
+            continue
+        if gitops.is_ancestor(main, tip, target_tip):
+            reasons.append(
+                f"{source.folder} has already been accepted into the target "
+                "on its own")
+    if reasons:
+        return tuple(reasons)
+
+    candidate = build_batch_candidate(
+        main, target_tip,
+        [(source.folder, source.source_tip) for source in receipt.sources])
+    if not candidate.ok:
+        return (f"rebuilt integration of {candidate.conflict_folder} conflicts: "
+                + ", ".join(candidate.conflicts),)
+    for source, tree in zip(receipt.sources, candidate.step_trees):
+        if tree != source.step_tree:
+            reasons.append(
+                f"rebuilt step tree for {source.folder} is {tree}, not the "
+                f"recorded {source.step_tree}")
+            break
+    if not reasons and candidate.step_trees[-1] != receipt.final_tree:
+        reasons.append(
+            f"rebuilt final tree is {candidate.step_trees[-1]}, not the recorded "
+            f"{receipt.final_tree}")
+    return tuple(reasons)
+
+
+def batch_receipt_is_current(cfg: Config,
+                             receipt: BatchVerificationReceipt) -> bool:
+    """True only for a PASSED batch receipt with no staleness reason left."""
+    if receipt.status != "PASSED":
+        return False
+    return not batch_receipt_staleness(cfg, receipt)
