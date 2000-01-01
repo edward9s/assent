38b4ec9bf1efa38d0968238650bcf688fbb64701
edward9s <277983297+edward9s@users.noreply.github.com>
2000-01-01 01:03:00 +0000

    auto(opensource01/t007): Translate clean and reject command domains

--- agents/reject.py
+++ agents/reject.py
@@ -1,4 +1,4 @@
-"""人工裁決駁回工作資料夾的實作與任務狀態。"""
+"""Implementation of manual-decision rejection for a task folder and its task statuses."""
 from __future__ import annotations
 
 from pathlib import Path
@@ -10,7 +10,7 @@ from agents.plan import Plan, append_entry, set_status
 
 
 def _remove_empty_container(path: Path) -> None:
-    """worktree 容器已空時移除；非空或移除失敗皆保留。"""
+    """Remove the worktree container once empty; retained if non-empty or removal fails."""
     try:
         path.parent.rmdir()
     except OSError:
@@ -18,10 +18,12 @@ def _remove_empty_container(path: Path) -> None:
 
 
 def reject_folder(cfg: Config) -> int:
-    """封存後強制清除 worktree 與同前綴分支，並把任務改回 TODO。
+    """Archive, then force-remove the worktree and same-prefix branches, and reset tasks to TODO.
 
-    鎖忙、鎖缺、任務檔預檢或任何 Git 步驟失敗一律回傳 1。任務檔必須在
-    破壞性 Git 操作前完整解析；Git 現場全數清除成功後才重置任務狀態。
+    Returns 1 for a busy lock, missing lock, task-file precheck failure, or any Git
+    step failure. Task files must parse completely before any destructive Git
+    operation; task statuses are reset only after the entire Git scene has been
+    cleared successfully.
     """
     name = cfg.tasks_name
     path = gitops.worktree_path(cfg.root, name)
@@ -30,21 +32,23 @@ def reject_folder(cfg: Config) -> int:
         with probe_lock(cfg.tasks_dir, name):
             return _reject_locked(cfg, path)
     except LockBusy as e:
-        print(f"{name}:駁回中止(run 進行中):{e}")
+        print(f"{name}: reject aborted (a run is in progress): {e}")
         return 1
     except (LockMissing, AgentsError) as e:
-        print(f"{name}:駁回中止({e})")
+        print(f"{name}: reject aborted ({e})")
         return 1
 
 
 def _reject_locked(cfg: Config, path: Path) -> int:
-    """持鎖後先預檢任務，再清 Git 現場，最後重置任務狀態。"""
+    """With the lock held, precheck the task files first, then clear the Git scene,
+    and finally reset task statuses."""
     root = cfg.root
     name = cfg.tasks_name
     try:
         plan = Plan.parse(cfg.tasks_dir)
     except AgentsError as e:
-        print(f"{name}:駁回中止(任務檔無法解析:{e}),Git 現場未變動")
+        print(f"{name}: reject aborted (task files could not be parsed: {e}), "
+              "Git scene unchanged")
         return 1
 
     evidence: list[str] = []
@@ -52,58 +56,64 @@ def _reject_locked(cfg: Config, path: Path) -> int:
         branches = gitops.branches_with_prefix(root, cfg.branch_prefix)
         if path.exists():
             if not gitops.is_repo_worktree(root, path):
-                print(f"{name}:駁回中止(固定路徑不是本 repo 的有效 worktree:"
-                      f"{path})")
+                print(f"{name}: reject aborted (fixed path is not a valid "
+                      f"worktree of this repo: {path})")
                 return 1
-            # 比照 engine 的 wip 檢查點哲學：先把未提交變更封存進 commit，
-            # 之後的 worktree remove 就不需要 force。
+            # Following the same wip-checkpoint philosophy as the engine: archive
+            # uncommitted changes into a commit first, so the later worktree remove
+            # does not need force.
             if gitops.commit_if_dirty(
-                    path, f"wip({name}): 駁回封存,保全未提交變更",
+                    path, f"wip({name}): reject archive, preserving uncommitted changes",
                     cfg.git_excludes):
-                print(f"{name}:未提交變更已封存為 wip commit")
+                print(f"{name}: uncommitted changes archived as a wip commit")
             head = gitops.commit_of(path, "HEAD")
             evidence.append(f"worktree HEAD {head}")
             gitops.remove_worktree(root, path)
             _remove_empty_container(path)
-            print(f"{name}:已除 worktree {path}(HEAD {head})")
+            print(f"{name}: removed worktree {path} (HEAD {head})")
         else:
-            print(f"{name}:worktree 不存在,繼續駁回分支")
+            print(f"{name}: worktree does not exist, continuing to reject branches")
 
-        # 非同前綴分支一概不碰；完整 tip hash 同時顯示於終端，並在成功後
-        # 寫入每個被重置任務的 rejected 日誌供日後救援。
+        # Branches without this prefix are never touched; the full tip hash is shown
+        # in the terminal and, after success, written into each reset task's rejected
+        # journal entry for later recovery.
         for branch in branches:
             tip = gitops.commit_of(root, branch)
             gitops.delete_branch_force(root, branch)
-            evidence.append(f"分支 {branch} tip {tip}")
-            print(f"  分支 {branch}(tip {tip}):已刪(僅 gc 期限內可用 hash 救回)")
+            evidence.append(f"branch {branch} tip {tip}")
+            print(f"  branch {branch} (tip {tip}): deleted (recoverable by hash "
+                  "only within the gc grace period)")
     except AgentsError as e:
-        print(f"{name}:駁回中止(Git 步驟失敗:{e}),任務檔未重置")
+        print(f"{name}: reject aborted (Git step failed: {e}), task files not reset")
         return 1
     return _reset_rejected_tasks(cfg, plan, evidence)
 
 
 def _reset_rejected_tasks(cfg: Config, plan: Plan,
                           evidence: list[str]) -> int:
-    """把 DONE/WIP/BLOCKED 改回 TODO，並以 r 檔保存完整 Git 存證。"""
+    """Reset DONE/WIP/BLOCKED back to TODO, and preserve full Git evidence in the r file."""
     name = cfg.tasks_name
     reset = 0
-    detail = "刪除前 Git 存證:\n" + (
-        "\n".join(evidence) if evidence else "沒有 worktree 或同前綴分支")
+    detail = "Git evidence before deletion:\n" + (
+        "\n".join(evidence) if evidence else "no worktree or same-prefix branches")
     try:
         for task in plan.tasks:
-            # SKIP 是人的明示放棄，TODO 本來就待做，駁回都不推翻。
+            # SKIP is an explicit human decision to abandon; TODO is already
+            # pending — reject does not override either.
             if task.status not in ("DONE", "WIP", "BLOCKED"):
                 continue
             set_status(task.path, "TODO")
             append_entry(
                 task.journal_path, by="scheduler", event="rejected",
-                summary=f"人工裁決駁回,調度器把狀態 {task.status} 改回 TODO",
+                summary=f"Manual-decision reject; scheduler reset status "
+                        f"{task.status} back to TODO",
                 detail=detail)
-            print(f"  任務 {task.id}:{task.status} -> TODO")
+            print(f"  task {task.id}: {task.status} -> TODO")
             reset += 1
     except (AgentsError, OSError) as e:
-        print(f"{name}:任務檔重置中斷({e}),請重跑 agents reject {name}")
+        print(f"{name}: task-file reset interrupted ({e}), rerun agents reject {name}")
         return 1
-    print(f"{name}:駁回完成(重置 {reset} 個任務為 TODO)。"
-          "視需要修訂任務檔後 agents run 重做;agents report 可更新報告。")
+    print(f"{name}: reject complete ({reset} task(s) reset to TODO). "
+          "Revise task files as needed then rerun with agents run; agents report "
+          "can refresh the report.")
     return 0
